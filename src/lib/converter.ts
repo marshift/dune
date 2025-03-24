7b6a91cb8a5d5e0f042aa66d2cdf0eb8f722b9e2
marshift <marshift@riseup.net>
2025-03-24 02:51:48 +0000

    converter: implement `element` and `text`, support expressions

--- src/lib/converter.ts
+++ src/lib/converter.ts
@@ -1,13 +1,24 @@
 import { Node } from "npm:kdljs";
+import { Context, evaluate, template } from "./expressions.ts";
 import html from "./html.ts";
 import { Parser } from "./parser.ts";
 
+function assertValueSize(node: Node, expected: number) {
+	if (node.values.length !== expected) {
+		throw new Error(
+			`Incorrect number of values passed to node of type \"${node.name}\", expected ${expected} but got ${node.values.length}`,
+		);
+	}
+}
+
 export class Converter {
 	private parser: Parser;
+	private ctx: Context;
 	public output = "";
 
-	constructor(parser: Parser) {
+	constructor(parser: Parser, ctx: Context) {
 		this.parser = parser;
+		this.ctx = ctx;
 
 		const root = this.parser.querySingle("page");
 		if (!root) throw new Error("Missing page root");
@@ -17,10 +28,32 @@ export class Converter {
 
 	private walk = (nodes: Node[], parent?: Node) => nodes.forEach((child) => this.visit(child, parent));
 	private visit(node: Node, parent?: Node) {
+		const values = node.values.map(String);
+		const properties = Object.fromEntries(
+			Object.entries(node.properties).map(([k, v]) => [
+				k,
+				template(String(v), this.ctx),
+			]),
+		);
+
 		switch (node.name) {
+			case "text": {
+				assertValueSize(node, 1);
+				this.output += template(values[0], this.ctx);
+				break;
+			}
+			case "element": {
+				assertValueSize(node, 1);
+				const name = evaluate(values[0], this.ctx, true);
+
+				this.output += html.open(name, properties);
+				this.walk(node.children, node);
+				this.output += html.close(name);
+				break;
+			}
 			default: {
-				this.output += html.open(node.name, node.properties);
-				this.output += node.values.map(String).join("\n");
+				this.output += html.open(node.name, properties);
+				this.output += values.map((v) => template(v, this.ctx)).join("\n");
 				this.walk(node.children, node);
 				this.output += html.close(node.name);
 
