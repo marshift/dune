2496d399f91197a5761bf9e43ce60fb9351467c6
marshift <marshift@riseup.net>
2025-03-30 04:58:19 +0100

    adapters: add basic JSON adapter

---
+++ src/lib/adapters/json.ts
@@ -0,0 +1,13 @@
+import { DuneNode } from "../parser.ts";
+import { Adapter } from "./base.ts";
+
+export class JSONAdapter extends Adapter {
+	private pretty? = false;
+
+	constructor(pretty?: boolean) {
+		super();
+		this.pretty = pretty;
+	}
+
+	override process = (ast: DuneNode[]) => JSON.stringify(ast, null, this.pretty ? 4 : 0);
+}
