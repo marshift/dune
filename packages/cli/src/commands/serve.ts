fe1bc1a7450727d877ae48500da79329a9da3caa
marshift <marshift@riseup.net>
2025-07-22 03:47:30 +0100

    cli: implement initial dev mode for ssr
    with hot reloading!

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,14 +1,17 @@
 import type { ArgusContext } from "@marshift/argus";
-import { join } from "node:path";
 import type { DuneConfig } from "../lib/config";
-import { createSSRServer } from "../lib/ssr";
+import { createDevServer, createSSRServer } from "../lib/ssr";
 import { Command } from "./base";
 
 export class ServeCommand extends Command {
 	override name = "serve";
 	override execute({ templateDir, staticDir }: DuneConfig, ctx: ArgusContext): Promise<void> {
 		const port = ctx.getOptionalArg(/--port|-p/) ?? 1413;
-		const server = createSSRServer(join(templateDir, "./pages/"), staticDir);
+		const dev = ctx.hasOptionalArg(/--dev|-d/);
+		const server = (dev ? createDevServer : createSSRServer)({
+			templateDir,
+			staticDir,
+		});
 
 		return Promise.resolve(
 			void server.listen(port, () => {
--- packages/cli/src/lib/ssr.ts
+++ packages/cli/src/lib/ssr.ts
@@ -1,29 +1,91 @@
 import { HTMLAdapter, Parser } from "@dunejs/core";
+import { watch } from "chokidar";
 import { readFile } from "node:fs/promises";
-import { createServer } from "node:http";
-import { join } from "node:path";
+import { createServer, IncomingMessage, ServerResponse } from "node:http";
+import { dirname, join } from "node:path";
 import { pathToFileURL } from "node:url";
 
-export const createSSRServer = (pagesDir: string, staticDir: string) =>
-	createServer(async (req, res) => {
-		let { pathname } = new URL(`http://dune.rocks${req.url}`);
-		if (pathname === "/") pathname = "index";
+interface ServerOptions {
+	templateDir: string;
+	staticDir: string;
+}
 
-		try {
-			const parser = await Parser.for(pathToFileURL(join(pagesDir, pathname + ".kdl")));
-			const content = parser.convert(new HTMLAdapter());
+function getPathname(req: IncomingMessage) {
+	const { pathname } = new URL(`http://dune.rocks${req.url}`);
+	if (pathname.endsWith("/")) {
+		const dir = dirname(pathname);
+		return `${dir}/index`;
+	} else {
+		return pathname;
+	}
+}
 
-			res.writeHead(200);
-			return res.end(content);
-		} catch { /* Empty block */ }
+// TODO: Something like onError
+async function handleFile(
+	{ templateDir, staticDir }: ServerOptions,
+	res: ServerResponse,
+	pathname: string,
+	onKDLDocument: (parser: Parser) => string,
+) {
+	try {
+		const parser = await Parser.for(pathToFileURL(join(templateDir, "pages", pathname + ".kdl")));
+		const content = onKDLDocument(parser);
 
-		try {
-			const content = await readFile(join(staticDir, pathname));
+		res.writeHead(200);
+		return res.end(content);
+	} catch (e) {}
 
-			res.writeHead(200);
-			return res.end(content);
-		} catch { /* Empty block */ }
+	try {
+		const content = await readFile(join(staticDir, pathname));
+		res.writeHead(200);
+		return res.end(content);
+	} catch { /* Empty block */ }
 
-		res.writeHead(404);
-		return res.end();
+	res.writeHead(404);
+	return res.end();
+}
+
+const hotReloadNodes = new Parser(`
+page {
+	head {
+		script #"""
+		const source = new EventSource("/_hot");
+		source.onmessage = () => document.location.reload();
+		"""#
+	}
+}
+`).toAST();
+
+export const createDevServer = (opts: ServerOptions) => {
+	const subscribers = new Set<ServerResponse>();
+	const watcher = watch([opts.templateDir, opts.staticDir], { ignoreInitial: true });
+	watcher.on("all", (event) => subscribers.forEach((s) => s.write(`data: ${event}\n\n`)));
+
+	return createServer(async (req, res) => {
+		const pathname = getPathname(req);
+
+		if (pathname === "/_hot") {
+			subscribers.add(res);
+			req.on("close", () => subscribers.delete(res));
+
+			return res.writeHead(200, {
+				"content-type": "text/event-stream",
+				"connection": "keep-alive",
+				"cache-control": "no-cache",
+			});
+		}
+
+		await handleFile(opts, res, pathname, (parser) => {
+			const ast = parser.toAST();
+			const head = ast
+				.filter((node) => node.type === "element")
+				.find((node) => node.name === "head");
+
+			(!head ? ast : head.body).unshift(...hotReloadNodes);
+			return new HTMLAdapter().process(ast);
+		});
 	});
+};
+
+export const createSSRServer = (opts: ServerOptions) =>
+	createServer((req, res) => handleFile(opts, res, getPathname(req), (parser) => parser.convert(new HTMLAdapter())));
